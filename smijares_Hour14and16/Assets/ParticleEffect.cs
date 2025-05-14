e62e78e27d245af36658f65b83539f5a3bf71c07
StickSam11 <sammijares849@gmail.com>
2025-05-14 14:04:46 -0700

    Hour 14 and 16 exercises

---
+++ smijares_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,10 @@
+// I changed the color of the particles
+
+// I changed the speed
+
+// I added two different colors rather than just one
+
+// Modified the gravity
+
+// Added a max particle of 5000
+
---
+++ smijares_Hour14and16/Assets/UserInterface.cs
@@ -0,0 +1,5 @@
+// I changed the color and size of the text as well as wrote something different
+
+// I added a new picture to the canvas
+
+// I made the button change color when pressed
